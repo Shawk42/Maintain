529d4f4a3a6c39e8c449fdf0267fef83877fd31e
Deryk <derahner@outlook.com>
2019-03-27 23:48:24 -0700

    Filter Added, Noticed weird anamoly in data

--- CSVRead.py
+++ CSVRead.py
@@ -2,6 +2,8 @@ import numpy as np
 from numpy import genfromtxt
 import matplotlib.pyplot as plt
 import time as timepack
+from scipy import signal
+
 
 start_time = timepack.time()
 print("STATUS - Starting")
@@ -14,11 +16,15 @@ x_1 = data_1[:,1]
 y_1 = data_1[:,2]
 z_1 = data_1[:,3]
 
+
+
 data_2 = genfromtxt('MondayTest.csv',delimiter=',')
 time_2 = data_2[:,0]
 x_2 = data_2[:,1]
 y_2 = data_2[:,2]
 z_2 = data_2[:,3]
+
+
 
 #Data Joining
 time = np.append(time_1,time_2)
@@ -32,52 +38,28 @@ print("STATUS - Data import successfull",round(intermed_time-start_time,2),"Seco
 
 """RESULTANT VECTOR"""
 r = (x**2+y**2+z**2)**0.5
-r_avg = np.average(r)
-dev = abs(r-r_avg)
-dev_avg = np.average(dev)
-R = np.array([])
-F = np.array([])
-t_F = 0
-t_Fprev = 0
-T = np.array([])
+
 
 print("STATUS - Resultant vector found",round(timepack.time()-intermed_time,2),"Seconds required")
 intermed_time = timepack.time()
 
-#Loop Analysis
 #Filtering
-for i in range(0,np.size(time)):
-    if False :
-        R = np.append(R,r_avg)
-        F = np.append(F,0)
-    else:
-        R = np.append(R,r.item(i))
-        F = np.append(F,1)
+b,a = signal.butter(1,0.1,'low')
+R = signal.filtfilt(b,a,r)
 
 
-print("STATUS - Filter Loop Complete",round(timepack.time()-intermed_time,2),"Seconds required")
+print("STATUS - Filtering Complete",round(timepack.time()-intermed_time,2),"Seconds required")
 intermed_time = timepack.time()
 
-for i in range(0,np.size(time)):
-    if F.item(i) == 1:
-        t_F = time.item(i)
-        t_Fdel = t_F - t_Fprev
-        t_F = t_Fprev
-        T = np.append(T,t_Fdel)
-    else:
-        "w"
-
-
-print("STATUS - Error Frequency Loop Complete",round(timepack.time()-intermed_time,2),"Seconds required")
-intermed_time = timepack.time()
 
 print("STATUS - Outputs")
 print("Total Computation time")
 print("Data Points",np.size(time))
-print("r_avg",r_avg)
-print("dev_avg",dev_avg)
 
+
+plt.plot(time,r)
 plt.plot(time,R)
+plt.legend(("Orginal","New"))
 plt.show()
 
 
